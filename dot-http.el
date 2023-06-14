5a3d11220ce722d23c1e6ee046caa7f48ca0081b
Michael Schlösser <mail@michael-schloesser.de>
2023-06-14 21:18:35 +0200

    Updated license text in main file

--- dot-http.el
+++ dot-http.el
@@ -9,9 +9,18 @@
 
 ;; This file is not part of GNU Emacs.
 
-;; This file is free software…
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
 
-;; along with this file.  If not, see <https://www.gnu.org/licenses/>.
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;;;
